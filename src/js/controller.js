6bbeb9d822097ca84638403112b0b403a7738454
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-07-19 06:55:54 +0200

    Loading Recipe from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,6 +12,28 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
-console.log('a7a');
-console.log('a7a');
-console.log('a7a');
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message}`);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      cookingTime: recipe.cooking_time,
+      imgUrl: recipe.img_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      title: recipe.title,
+    };
+    console.log(recipe);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+showRecipe();
